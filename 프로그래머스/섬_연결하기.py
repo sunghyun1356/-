bff5eac4dac463347203eda01f9bc253782b76d0
sunghyun1356 <sunghyun1356@naver.com>
2023-12-30 15:33:50 +0900

    12.30

--- 프로그래머스/섬_연결하기.py
+++ 프로그래머스/섬_연결하기.py
@@ -1,34 +1,37 @@
-from collections import deque
+def find(parent, i):
+    if parent[i] == i:
+        return i
+    parent[i] = find(parent, parent[i])
+    return parent[i]
 
-def DFS(graph, start, visited):
-    q = deque([start])
-    while q:
-        now = q.popleft()
-        if not visited[now]:
-            visited[now] = True
-            for neighbor, cost in graph[now]:
-                if not visited[neighbor]:
-                    q.append(neighbor)
-
-def solution(n,costs):
+def union(parent, rank, x, y):
+    xroot = find(parent, x)
+    yroot = find(parent, y)
+    
+    if rank[xroot] < rank[yroot]:
+        parent[xroot] = yroot
+    elif parent[xroot] == parent[yroot]:
+        rank[yroot]+=1
+    else:
+        parent[yroot] = xroot
+    
+def solution(n, costs):
+    costs.sort(key = lambda x : x[2])
+    parent = [i for i in range(n)]
+    
+    rank = [0] * n
+    
     answer = 0
-    graph ={}
-    for i in range(n):
-        graph[i] = []
-        
-    costs = sorted(costs, key=lambda x : x[2])
-    
+    edges = 0
     for i in costs:
-        if not(graph[i[0]] and graph[i[1]]):
-            graph[i[0]].append((i[1],i[2]))
-            graph[i[1]].append((i[0],i[2]))
-            
-    visited = [False] *n
-    DFS(graph, 0, visited)
-    
+        if edges == n-1:
+            break
+        
+        x = find(parent, i[0])
+        y = find(parent, i[1])
+        
+        if x!=y:
+            union(parent, rank, x, y)
+            answer +=i[2]
+        edges+=1
     return answer
-
-n =4
-costs = [[0, 1, 1], [0, 2, 2], [1, 2, 5], [1, 3, 1], [2, 3, 8]]
-result = solution(n, costs)
-print(result)
